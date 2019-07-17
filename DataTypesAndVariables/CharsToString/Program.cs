8c80f92665d578840b79d9ad95f7a4462410fb63
delev <delyan1@mail.bg>
2019-07-17 13:50:34 +0300

    Lab problems solutions

---
+++ DataTypesAndVariables/CharsToString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            for (int i = 0; i < 3; i++)
+            {
+                name+= char.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ DataTypesAndVariables/RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= count; i++)
+            {
+                int num = i;
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                bool isSpecial= (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", i, isSpecial);
+            }
+        }
+    }
+}
