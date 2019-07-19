619bb60a575c6fa3997007fe122705ee318a1c4c
delev <delyan1@mail.bg>
2019-07-19 12:46:45 +0300

    add regex EXS half homework

---
+++ C# Fundamentals/Regular Expressions/RegularExpressionsEXS/Furniture/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bought furniture: ");
+            double totalMoney = 0;
+            string command;
+
+            while ((command = Console.ReadLine()) != "Purchase")
+            {
+                string pattern = @">>(?<name>.+)<<(?<price>\d+\.?\d+)!(?<count>\d+)";
+                string input = command;
+
+                if (Regex.IsMatch(input, pattern))
+                {
+                    var matchedInput = Regex.Matches(input, pattern);
+
+                    foreach (Match match in matchedInput)
+                    {
+                        Console.WriteLine(match.Groups["name"].Value);
+                        totalMoney += double.Parse(match.Groups["price"].Value)*int.Parse(match.Groups["count"].Value);
+                    }
+                }
+            }
+            Console.WriteLine($"Total money spend: {totalMoney:f2}");
+        }
+    }
+}
---
+++ C# Fundamentals/Regular Expressions/RegularExpressionsEXS/Race/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(", ").ToList();
+            var namesAndPoints = new Dictionary<string, int>();
+            string command;
+
+            while((command=Console.ReadLine())!="end of race")
+            {
+                string lettersPattern = @"[a-z]|[A-Z]";
+                var letters = Regex.Matches(command, lettersPattern);
+                string name = "";
+
+                foreach (Match letter in letters)
+                {
+                    name += letter.Value;
+                }
+                if (names.Contains(name))
+                {
+                    string digitsPattern = @"[0-9]";
+                    var digits = Regex.Matches(command, digitsPattern);
+                    int points = 0;
+
+                    foreach (Match digit in digits)
+                    {
+                        points += int.Parse(digit.Value);
+                    }
+                    if (!namesAndPoints.ContainsKey(name))
+                    {
+                        namesAndPoints[name] = 0;
+                    }
+                        namesAndPoints[name] += points;
+                }
+            }
+            var winners = namesAndPoints
+                .OrderByDescending(x => x.Value)
+                .Take(3)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value)
+                .Keys
+                .ToList();
+
+            Console.WriteLine($"1st place: {winners[0]}");
+            Console.WriteLine($"2nd place: {winners[1]}");
+            Console.WriteLine($"3rd place: {winners[2]}");
+        }
+    }
+}
