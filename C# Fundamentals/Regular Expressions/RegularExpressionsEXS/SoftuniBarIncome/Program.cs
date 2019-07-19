fff3a8ecfa8cbd40582994a43d21d0f6ad0cb0c2
delev <delyan1@mail.bg>
2019-07-19 12:49:26 +0300

    fixing softuniBarIncome problem

--- C# Fundamentals/Regular Expressions/RegularExpressionsEXS/SoftuniBarIncome/Program.cs
+++ C# Fundamentals/Regular Expressions/RegularExpressionsEXS/SoftuniBarIncome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace SoftuniBarIncome
 {
@@ -6,7 +7,26 @@ namespace SoftuniBarIncome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"%(?<name>[A-Z][a-z]+)%[^\$\|\%\.]*<(?<product>\w+)>[^\$\|\%\.]*\|(?<count>\d+)\|[^\$\|\%\.\d]*(?<price>\d+(\.\d+)?)\$";
+            double totalIncome = 0;
+            string command;
+
+            while ((command = Console.ReadLine()) != "end of shift")
+            {
+                if (Regex.IsMatch(command, pattern))
+                {
+                    var matchedOrder = Regex.Match(command, pattern);
+
+                    string name = matchedOrder.Groups["name"].Value;
+                    string product = matchedOrder.Groups["product"].Value;
+                    int count = int.Parse(matchedOrder.Groups["count"].Value);
+                    double price = double.Parse(matchedOrder.Groups["price"].Value);
+
+                    Console.WriteLine($"{name}: {product} - {count * price:f2}");
+                    totalIncome += count * price;
+                }
+            }
+            Console.WriteLine($"Total income: {totalIncome:f2}");
         }
     }
 }
