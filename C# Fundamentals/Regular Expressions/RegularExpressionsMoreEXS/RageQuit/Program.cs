4d15f3c7710f94adfe364e71670a61f23a60f3a9
delev <delyan1@mail.bg>
2019-07-20 16:25:33 +0300

    Add rageQuit and winningTicket solutions

---
+++ C# Fundamentals/Regular Expressions/RegularExpressionsMoreEXS/RageQuit/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ExamPreparation
+{
+    public sealed class Preparation
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            var numbers = Regex.Matches(input, @"\d+").Cast<Match>().Select(m => m.Value).Select(int.Parse).ToArray();
+            var word = Regex.Matches(input, @"\D+").Cast<Match>().Select(m => m.Value).ToArray();
+
+            var symbols = new List<char>();
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                string currentWord = word[i].ToUpper();
+                int currentNumber = numbers[i];
+
+                for (int j = 0; j < currentNumber; j++)
+                {
+                    sb.Append(currentWord);
+                }
+                if (currentNumber > 0)
+                {
+                    for (int k = 0; k < currentWord.Length; k++)
+                    {
+                        if (!symbols.Contains(currentWord[k]))
+                        {
+                            symbols.Add(currentWord[k]);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Unique symbols used: {symbols.Count}");
+            Console.WriteLine(sb);
+           
+            ////AnotherSolution
+            
+            // string input = Console.ReadLine();
+            // string pattern = @"(([^\d]+)(\d+))";
+
+            // var matches = Regex.Matches(input, pattern);
+            // StringBuilder sb = new StringBuilder();
+
+            // foreach (Match match in matches)
+            // {
+            //     string message = match.Groups[2].Value;
+            //     int repeats = int.Parse(match.Groups[3].Value);
+
+            //     for (int i = 0; i < repeats; i++)
+            //     {
+            //         sb.Append(message.ToUpper());
+            //     }
+            // }
+
+            // int count = sb.ToString().Distinct().Count();
+
+            // Console.WriteLine($"Unique symbols used: {count}");
+            // Console.WriteLine(sb);
+        }
+    }
+}
---
+++ C# Fundamentals/Regular Expressions/RegularExpressionsMoreEXS/WinningTicket/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace WinningTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tickets = Regex.Split(Console.ReadLine(), @"\s*,\s+");
+
+            string validTicketPattern = @"^.{20}$";
+            string matchTicketPattern = @"\${6,10}|@{6,10}|\#{6,10}|\^{6,10}";
+
+            for (int i = 0; i < tickets.Length; i++)
+            {
+                string currentTicket = tickets[i];
+
+                if (Regex.IsMatch(currentTicket, validTicketPattern))
+                {
+                    string leftSide = currentTicket.Substring(0, 10);
+                    string rightSide = currentTicket.Substring(10, 10);
+
+                    var leftSideMatch = Regex.Match(leftSide, matchTicketPattern);
+                    var rightSideMatch = Regex.Match(rightSide, matchTicketPattern);
+
+                    bool isBothSidesEqual = false;
+
+                    if (leftSideMatch.Success && rightSideMatch.Success && leftSideMatch.ToString() == rightSideMatch.ToString())
+                    {
+                        isBothSidesEqual = true;
+                    }
+                    else if(leftSideMatch.Success && rightSideMatch.Success && leftSideMatch.ToString() != rightSideMatch.ToString())
+                    {
+                        if (leftSideMatch.ToString().Contains(rightSideMatch.ToString()))
+                        {
+                            isBothSidesEqual = true;
+                        }
+                        else if (rightSideMatch.ToString().Contains(leftSideMatch.ToString()))
+                        {
+                            isBothSidesEqual = true;
+                        }
+                    }
+                    if (isBothSidesEqual)
+                    {
+                        char symbol = leftSideMatch.ToString()[0];
+                        int count = Math.Min(leftSideMatch.ToString().Length,rightSideMatch.ToString().Length);
+
+                        if (count == 10)
+                        {
+                            Console.WriteLine($"ticket \"{currentTicket}\" - {count}{symbol} Jackpot!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ticket \"{currentTicket}\" - {count}{symbol}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{currentTicket}\" - no match");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid ticket");
+                }
+            }
+        }
+    }
+}
