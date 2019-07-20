6a6cae4a6fe85f330fd434ef0594d7ac3753170b
delev <delyan1@mail.bg>
2019-07-20 12:58:04 +0300

    add extractEmails solution

---
+++ C# Fundamentals/Regular Expressions/RegularExpressionsEXS/ExtractEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(^|(?<=\s))([a-z]|[0-9])+[\.\-_]?[a-z]*\d*@[a-z]+-?[a-z]*\.[a-z]+\.?[a-z]+";
+
+            var matchedWords = Regex.Matches(input, pattern);
+
+            foreach (Match word in matchedWords)
+            {
+                Console.WriteLine(word.Value);
+            }
+        }
+    }
+}
