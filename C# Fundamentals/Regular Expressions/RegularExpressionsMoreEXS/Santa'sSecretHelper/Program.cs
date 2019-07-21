6db2df9f1f13aff2824817963174392786e0c737
delev <delyan1@mail.bg>
2019-07-21 15:42:45 +0300

    add Santa'sSecretHelper solution

---
+++ C# Fundamentals/Regular Expressions/RegularExpressionsMoreEXS/Santa'sSecretHelper/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Santa_sSecretHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+    
+            string validNamePattern = @"@([a-zA-Z]+)[^@\-!:>]*!([GN])!";
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string message = "";
+
+                for (int i = 0; i < command.Length; i++)
+                {
+                    message += (char)(command[i] - key);
+                }
+
+                var matchedName = Regex.Match(message, validNamePattern);
+
+                if (matchedName.Success)
+                {
+                    if (matchedName.Groups[2].ToString() == "G")
+                    {
+                        Console.WriteLine(matchedName.Groups[1]);
+                    }
+                }
+            }
+        }
+    }
+}
