3f620c4f472330b069fbc3341cb97ba4f2de3033
delev <delyan1@mail.bg>
2019-07-22 14:02:22 +0300

    add FinalExam14April2019Group2 solutions

---
+++ C# Fundamentals/Exams/FinalExamPreparation/FinalExam-14April2019Group2/PracticeSessions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeSessions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var roadsAndRacers = new Dictionary<string, List<string>>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandArgs = command.Split("->");
+
+                string action = commandArgs[0];
+                string road = commandArgs[1];
+
+                if (action == "Add")
+                {
+                    string racer = commandArgs[2];
+
+                    if (!roadsAndRacers.ContainsKey(road))
+                    {
+                        roadsAndRacers[road] = new List<string>();
+                    }
+                    roadsAndRacers[road].Add(racer);
+                }
+                else if (action == "Move")
+                {
+                    string racer = commandArgs[2];
+                    string nextRoad = commandArgs[3];
+
+                    if (roadsAndRacers[road].Contains(racer))
+                    {
+                        roadsAndRacers[road].Remove(racer);
+                        roadsAndRacers[nextRoad].Add(racer);
+                    }
+                }
+                else if (action == "Close")
+                {
+                    if (roadsAndRacers.ContainsKey(road))
+                    {
+                        roadsAndRacers.Remove(road);
+                    }
+                }
+            }
+            roadsAndRacers = roadsAndRacers
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            Console.WriteLine("Practice sessions: ");
+
+            foreach (var road in roadsAndRacers)
+            {
+                Console.WriteLine(road.Key);
+
+                foreach (var racer in road.Value)
+                {
+                    Console.WriteLine($"++{racer}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/FinalExamPreparation/FinalExam-14April2019Group2/TheIsleofManTTRace/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TheIsleofManTTRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string messagePattern = @"^([#$%*&])(?<name>[A-Za-z]+)\1=(?<lenght>\d+)!!(?<code>.+)$";
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+
+                var matchedMessage = Regex.Match(message, messagePattern);
+
+                if (matchedMessage.Success)
+                {
+                    string name = matchedMessage.Groups["name"].Value;
+                    string code = matchedMessage.Groups["code"].Value;
+                    int lenght = int.Parse(matchedMessage.Groups["lenght"].Value);
+
+                    if (code.Length != lenght)
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                    else
+                    {
+                        string decriptedCode = "";
+
+                        for (int i = 0; i < code.Length; i++)
+                        {
+                            decriptedCode += (char)(code[i] + lenght);
+                        }
+                        Console.WriteLine($"Coordinates found! {name} -> {decriptedCode}");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+            }
+        }
+    }
+}
