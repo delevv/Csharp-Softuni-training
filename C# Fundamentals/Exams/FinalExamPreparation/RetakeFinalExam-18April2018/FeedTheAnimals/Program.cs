0d5d72f1c388b8571adb36ed50a974a43a3eb0fc
delev <delyan1@mail.bg>
2019-07-22 12:56:54 +0300

    add RetakeFinalExam18April2018 solutions

---
+++ C# Fundamentals/Exams/FinalExamPreparation/RetakeFinalExam-18April2018/FeedTheAnimals/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FeedTheAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new Dictionary<string, int>();
+            var areas = new Dictionary<string, int>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Last Info")
+            {
+                string[] commandArgs = command.Split(":");
+
+                string action = commandArgs[0];
+                string name = commandArgs[1];
+                int food = int.Parse(commandArgs[2]);
+                string area = commandArgs[3];
+
+                if (action == "Add")
+                {
+                    if (!animals.ContainsKey(name))
+                    {
+                        animals[name] = food;
+
+                        if (!areas.ContainsKey(area))
+                        {
+                            areas[area] = 1;
+                        }
+                        else
+                        {
+                            areas[area]++;
+                        }
+                    }
+                    else
+                    {
+                        animals[name] += food;
+                    }
+                }
+                else if (action == "Feed")
+                {
+                    if (animals.ContainsKey(name))
+                    {
+                        animals[name] -= food;
+
+                        if (animals[name] <= 0)
+                        {
+                            Console.WriteLine($"{name} was successfully fed");
+                            animals.Remove(name);
+                            areas[area]--;
+                        }
+                    }
+                }
+            }
+            animals = animals
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            Console.WriteLine("Animals: ");
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.Key} -> {animal.Value}g");
+            }
+
+            areas = areas
+                .OrderByDescending(x => x.Value)
+                .Where(x => x.Value > 0)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            Console.WriteLine("Areas with hungry animals: ");
+
+            foreach (var area in areas)
+            {
+                Console.WriteLine($"{area.Key} : {area.Value}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/FinalExamPreparation/RetakeFinalExam-18April2018/TechnologyFundamentalsRetakeFinalExam-18April2018/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace AnimalSanctuary
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfMessages = int.Parse(Console.ReadLine());
+
+            string infoPattern = @"^n:(?<name>[^;]+);t:(?<kind>[^;]+);c--(?<country>[A-Za-z\s]+)$";
+            string cryptedPattern = @"[A-Za-z\s]";
+            string weightPattern = @"\d";
+
+            int totalWeight = 0;
+
+            for (int i = 0; i < countOfMessages; i++)
+            {
+                string message = Console.ReadLine();
+
+                var matchedMessage = Regex.Match(message, infoPattern);
+
+                if (matchedMessage.Success)
+                {
+                    string cryptedName = matchedMessage.Groups["name"].Value;
+                    string cryptedKind = matchedMessage.Groups["kind"].Value;
+                    string country = matchedMessage.Groups["country"].Value;
+
+                    var symbolsInName = Regex.Matches(cryptedName, cryptedPattern);
+                    var symbolsInKind = Regex.Matches(cryptedKind, cryptedPattern);
+
+                    string name = "";
+
+                    foreach (Match symbol  in symbolsInName)
+                    {
+                        name += symbol.Value;
+                    }
+                    string kind = "";
+
+                    foreach (Match symbol in symbolsInKind)
+                    {
+                        kind += symbol.Value;
+                    }
+                    Console.WriteLine($"{name} is a {kind} from {country}");
+
+                    var digitsSum = Regex.Matches(message, weightPattern).Cast<Match>().Select(m => m.Value).Select(int.Parse).Sum();
+
+                    totalWeight += digitsSum;
+                }
+            }
+            Console.WriteLine($"Total weight of animals: {totalWeight}KG");
+        }
+    }
+}
