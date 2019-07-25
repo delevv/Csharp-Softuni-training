059fa33745e1ec321fb7b206a92f8cbe9c0f9223
delev <delyan1@mail.bg>
2019-07-25 14:06:35 +0300

    add FinalExam-24July2019 solution

---
+++ C# Fundamentals/Exams/FinalExamPreparation/FinalExam-24July2019/Concert/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bands = new Dictionary<string, List<string>>();
+            var bandsAndTime = new Dictionary<string, int>();
+
+            string command;
+
+            while((command=Console.ReadLine())!="start of concert")
+            {
+                string[] commandArgs = command.Split("; ");
+
+                string action = commandArgs[0];
+                string bandName = commandArgs[1];
+
+                if (action == "Add")
+                {
+                    if (!bands.ContainsKey(bandName))
+                    {
+                        bands[bandName] = new List<string>();
+                    }
+                    string[] members = commandArgs[2].Split(", ");
+
+                    foreach (var member in members)
+                    {
+                        if (!bands[bandName].Contains(member))
+                        {
+                            bands[bandName].Add(member);
+                        }
+                    }
+                }
+                else if (action == "Play")
+                {
+                    int time = int.Parse(commandArgs[2]);
+                    if (!bandsAndTime.ContainsKey(bandName))
+                    {
+                        bandsAndTime[bandName] = 0;
+                    }
+                    bandsAndTime[bandName] += time;
+                }
+            }
+            Console.WriteLine($"Total time: {bandsAndTime.Values.Sum()}");
+
+            bandsAndTime = bandsAndTime
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            foreach (var band in bandsAndTime)
+            {
+                Console.WriteLine($"{band.Key} -> {band.Value}");
+            }
+
+            string selectedBand = Console.ReadLine();
+
+            Console.WriteLine(selectedBand);
+
+            foreach (var member in bands[selectedBand])
+            {
+                Console.WriteLine($"=> {member}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/FinalExamPreparation/FinalExam-24July2019/SongEncryption/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SongEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPattern = @"^(?<artist>[A-Z][a-z'\s]+):(?<song>[A-Z\s]+)$";
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                var validInput = Regex.Match(command, inputPattern);
+
+                if (validInput.Success)
+                {
+                    string artist = validInput.Groups["artist"].Value;
+                    string song = validInput.Groups["song"].Value;
+                    int key = artist.Length;
+
+                    string artistAndSong = artist + ':' + song;
+                    string cryptedInfo = "";
+
+                    foreach (var symbol in artistAndSong)
+                    {
+                        char currentSymbol = symbol;
+
+                        if (currentSymbol + key > 'z')
+                        {
+                            int remainingLenght = key - ('z' - currentSymbol) - 1;
+                            currentSymbol = (char)('a' + remainingLenght);
+                        }
+                        else if (currentSymbol + key > 'Z' && symbol < 'a')
+                        {
+                            int remainingLenght = key - ('Z' - currentSymbol) - 1;
+                            currentSymbol = (char)('A' + remainingLenght);
+                        }
+                        else if (currentSymbol == ':')
+                        {
+                            currentSymbol = '@';
+                        }
+                        else if (currentSymbol != ' ' && currentSymbol != '\'')
+                        {
+                            currentSymbol = (char)(currentSymbol + key);
+                        }
+                        cryptedInfo += currentSymbol;
+                    }
+                    Console.WriteLine($"Successful encryption: {cryptedInfo}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+        }
+    }
+}
