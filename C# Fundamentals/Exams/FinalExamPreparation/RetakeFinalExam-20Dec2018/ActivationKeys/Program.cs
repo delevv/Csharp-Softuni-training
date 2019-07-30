1c5b5624c3c5b3c9f8d209f190426588ace68ca0
delev <delyan1@mail.bg>
2019-07-30 14:37:34 +0300

    Add RetakeFinalExam-20Dec2018 solution

---
+++ C# Fundamentals/Exams/FinalExamPreparation/RetakeFinalExam-20Dec2018/ActivationKeys/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace ActivationKeys
+{
+    class Program
+    {
+        static string SplitCode(string text, int index, string symbol)
+        {
+            int startIndex = index;
+            for (int i = 1; i <= startIndex-1; i++)
+            {
+                text = text.Insert(index, "-");
+                index += startIndex + 1;
+            }
+            return text;
+        }
+        static string ChangeDigits(string text)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    result += 9 - (text[i] - '0');
+                }
+                else
+                {
+                    result += text[i];
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("&");
+            var codes = new List<string>();
+
+            string shortPattern = @"^[a-zA-Z0-9]{16}$";
+            string longPattern = @"^[a-zA-Z0-9]{25}$";
+
+            foreach (var code in input)
+            {
+                var shortMatch = Regex.Match(code, shortPattern);
+                var longMatch = Regex.Match(code, longPattern);
+
+                if (shortMatch.Success)
+                {
+                    string result = shortMatch.ToString().ToUpper();
+                    result = SplitCode(result, 4, "-");
+                    string decodedResult = ChangeDigits(result);
+                    codes.Add(decodedResult);
+                }
+                else if (longMatch.Success)
+                {
+                    string result = longMatch.ToString().ToUpper();
+                    result = SplitCode(result, 5, "-");
+                    string decodedResult = ChangeDigits(result);
+                    codes.Add(decodedResult);
+                }
+            }
+            Console.WriteLine(string.Join(", ", codes));
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/FinalExamPreparation/RetakeFinalExam-20Dec2018/VaporWinterSale/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VaporWinterSale
+{
+    class DlcAndPrice
+    {
+        public string DlcName { get; set; }
+        public double Price { get; set; }
+
+        public DlcAndPrice(string name,double price)
+        {
+            this.DlcName = name;
+            this.Price = price;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+
+            var gamesWithDlc = new Dictionary<string, DlcAndPrice>();
+            var games = new Dictionary<string, double>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Contains(":"))
+                {
+                    string[] info = input[i].Split(":");
+
+                    string game = info[0];
+                    string dlc = info[1];
+
+                    if (games.ContainsKey(game))
+                    {
+                        DlcAndPrice current = new DlcAndPrice(dlc, games[game] * 1.20);
+                        gamesWithDlc[game] = current;
+                        games.Remove(game);
+                    }
+                }
+                else
+                {
+                    string[] info = input[i].Split("-");
+
+                    string game = info[0];
+                    double price = double.Parse(info[1]);
+
+                    if (!games.ContainsKey(game))
+                    {
+                        games[game] = price;
+                    }
+                }
+            }
+            foreach (var game in gamesWithDlc.OrderBy(x=>x.Value.Price))
+            {
+                    Console.WriteLine($"{game.Key} - {game.Value.DlcName} - {game.Value.Price * 0.5:f2}");
+            }
+            foreach (var game in games.OrderByDescending(x=>x.Value))
+            {
+                Console.WriteLine($"{game.Key} - {game.Value*0.80:f2}");
+            }
+        }
+    }
+}
