6926c766dda839783b6b578803f30a4cf8e5df84
delev <delyan1@mail.bg>
2019-09-20 09:58:15 +0300

    Add MatchingBrackets solution

---
+++ C# Advanced/StacksAndQueues/MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expression = Console.ReadLine();
+
+            var openIndexes = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    openIndexes.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    var startIndex = openIndexes.Pop();
+                    var endIndex = i;
+
+                    Console.WriteLine(expression.Substring(startIndex,endIndex-startIndex+1));
+                }
+            }
+        }
+    }
+}
