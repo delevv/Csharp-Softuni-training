905a315cafae738a100afb14e00a45edd7ec389f
delev <delyan1@mail.bg>
2019-09-20 09:48:53 +0300

    Add SimpleCalculator solution

---
+++ C# Advanced/StacksAndQueues/SimpleCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expression = Console.ReadLine().Split().ToArray().Reverse();
+
+            var stack = new Stack<string>(expression);
+            var result = 0;
+
+            while (stack.Count > 0)
+            {
+                if (stack.Peek()=="+")
+                {
+                    stack.Pop();
+                    result += int.Parse(stack.Pop());
+                }
+                else if (stack.Peek() == "-")
+                {
+                    stack.Pop();
+                    result -= int.Parse(stack.Pop());
+                }
+                else
+                {
+                    result += int.Parse(stack.Pop());
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
