b61de222fe0334f3561a2c855121d6aa40bb8d65
delev <delyan1@mail.bg>
2019-09-24 10:04:29 +0300

    add BasicStackOperations solution

---
+++ C# Advanced/StacksAndQueues/StacksAndQueuesEXS/StacksAndQueuesEXS/BasicStackOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var info = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var elementsCount = info[0];
+            var elementsToPop = info[1];
+            var specialNumber = info[2];
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < elementsCount; i++)
+            {
+                stack.Push(elements[i]);
+            }
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                if (stack.Count != 0)
+                {
+                    stack.Pop();
+                }
+            }
+            if (stack.Contains(specialNumber))
+            {
+                Console.WriteLine("true");
+            }
+            else if(stack.Count>0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
