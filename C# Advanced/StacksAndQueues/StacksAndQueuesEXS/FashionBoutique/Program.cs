f5ab3003189cfbf229678fc2f0e30642fab6e0f6
delev <delyan1@mail.bg>
2019-09-24 11:43:18 +0300

    add FashionBoutique solution

---
+++ C# Advanced/StacksAndQueues/StacksAndQueuesEXS/FashionBoutique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clothesInBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var rackCapacity = int.Parse(Console.ReadLine());
+
+            var stack = new Stack<int>(clothesInBox);
+            var rackCount = 1;
+            var currentRack = rackCapacity;
+
+            while (stack.Count > 0)
+            {
+                var currentClothes = stack.Peek();
+
+                if (currentClothes <= currentRack)
+                {
+                    currentRack -= stack.Pop();
+                }
+                else
+                {
+                    rackCount++;
+                    currentRack = rackCapacity;
+                }
+            }
+            Console.WriteLine(rackCount);
+        }
+    }
+}
