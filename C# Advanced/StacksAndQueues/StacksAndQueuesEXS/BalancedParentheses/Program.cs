176bd0e0da6bda6dd493d4751c2c7cea5c46e2a1
delev <delyan1@mail.bg>
2019-09-24 14:56:08 +0300

    Add BalancedParentheses solution

---
+++ C# Advanced/StacksAndQueues/StacksAndQueuesEXS/BalancedParentheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequenceOfParantheses = Console.ReadLine();
+
+            var openBrackets = new Stack<char>();
+
+            if (sequenceOfParantheses.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            var isBalanced = true;
+
+            for (int i = 0; i < sequenceOfParantheses.Length; i++)
+            {
+                if (sequenceOfParantheses[i] == '(' || sequenceOfParantheses[i] == '[' || sequenceOfParantheses[i] == '{')
+                {
+                    openBrackets.Push(sequenceOfParantheses[i]);
+                }
+                else
+                {
+                    var open = openBrackets.Pop();
+                    var closed = sequenceOfParantheses[i];
+
+                    if (open == '(' && closed == ')')
+                    {
+                        isBalanced = true;
+                    }
+                    else if (open == '{' && closed == '}')
+                    {
+                        isBalanced = true;
+                    }
+                    else if (open == '[' && closed == ']')
+                    {
+                        isBalanced = true;
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                }
+            }
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
