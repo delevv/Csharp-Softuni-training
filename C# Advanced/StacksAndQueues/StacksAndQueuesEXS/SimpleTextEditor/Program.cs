1e571720150a5103e7a650b1de3cebf96d7e0d3f
delev <delyan1@mail.bg>
2019-09-25 16:30:08 +0300

    Add SimpleTextEditor solution

---
+++ C# Advanced/StacksAndQueues/StacksAndQueuesEXS/SimpleTextEditor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operationsCount = int.Parse(Console.ReadLine());
+            var text = new StringBuilder();
+            var stack = new Stack<string>();
+
+            for (int i = 0; i < operationsCount; i++)
+            {
+                var commandArgs = Console.ReadLine().Split();
+                var action = int.Parse(commandArgs[0]);
+                var lastText = text.ToString();
+
+                switch (action)
+                {
+                    case 1:
+                        stack.Push(lastText);
+                        text.Append(commandArgs[1]);
+                        break;
+                    case 2:
+                        var count = int.Parse(commandArgs[1]);
+                        stack.Push(lastText);
+                        text.Remove(text.Length - count, count);
+                        break;
+                    case 3:
+                        var index = int.Parse(commandArgs[1]);
+                        Console.WriteLine(text[index - 1]);
+                        break;
+                    case 4:
+                        text.Clear();
+                        text.Append(stack.Pop());
+                        break;
+                }
+            }
+        }
+    }
+}
