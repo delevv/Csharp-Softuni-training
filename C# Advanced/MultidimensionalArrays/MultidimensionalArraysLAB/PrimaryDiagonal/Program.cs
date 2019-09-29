ad14263fe71235345f472ed3439c4b6ca9c0dcb7
delev <delyan1@mail.bg>
2019-09-29 11:22:38 +0300

    Add PrimaryDiagonal solution

---
+++ C# Advanced/MultidimensionalArrays/MultidimensionalArraysLAB/PrimaryDiagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixSize = int.Parse(Console.ReadLine());
+
+            var matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                var matrixRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = matrixRow[col];
+                }
+            }
+            var colIndex = 0;
+            var sum = 0;
+            
+            for (int i = 0; i < matrixSize; i++)
+            {
+                sum += matrix[i, colIndex];
+                colIndex++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
