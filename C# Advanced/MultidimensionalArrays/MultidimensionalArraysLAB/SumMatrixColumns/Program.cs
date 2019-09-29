70d30fb0e51257dd9c5170a5ba63d87b777ec35f
delev <delyan1@mail.bg>
2019-09-29 11:11:50 +0300

    Add SumMatrixColumns solution

---
+++ C# Advanced/MultidimensionalArrays/MultidimensionalArraysLAB/SumMatrixColumns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var info = Console.ReadLine().Split(", ");
+            var rows = int.Parse(info[0]);
+            var cols = int.Parse(info[1]);
+
+            var array = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var rowNumbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = rowNumbers[j];
+                }
+            }
+            var sum = 0;
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    sum += array[j, i];
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+        }
+    }
+}
