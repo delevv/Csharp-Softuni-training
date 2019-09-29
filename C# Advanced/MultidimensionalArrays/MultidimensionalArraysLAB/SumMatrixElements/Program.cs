a9e79d044322f89d07737636d1ffd60bb55039ed
delev <delyan1@mail.bg>
2019-09-29 11:03:06 +0300

    Add MultidimensionalArrays folder Add SumMatrixElements solution

---
+++ C# Advanced/MultidimensionalArrays/MultidimensionalArraysLAB/SumMatrixElements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var info = Console.ReadLine().Split(", ");
+            var rows = int.Parse(info[0]);
+            var cols = int.Parse(info[1]);
+
+            var array = new int[rows, cols];
+            var sum = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                var rowNumbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = rowNumbers[j];
+                    sum += rowNumbers[j];
+                }
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
