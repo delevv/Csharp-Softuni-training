93c0d2d9ee1314004c8307e82f631354d7537437
delev <delyan1@mail.bg>
2019-10-02 11:17:54 +0300

    Add SnakeMoves solution

---
+++ C# Advanced/MultidimensionalArrays/MultidimensionalArraysEXS/JaggedArrayManipulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JaggedArrayManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ C# Advanced/MultidimensionalArrays/MultidimensionalArraysEXS/SnakeMoves/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixDimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var snake = Console.ReadLine();
+
+            var rows = matrixDimensions[0];
+            var cols = matrixDimensions[1];
+            var matrix = new char[rows, cols];
+            var snakeIndex = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        matrix[row, col] = snake[snakeIndex++];
+                        if (snakeIndex == snake.Length)
+                        {
+                            snakeIndex = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1)-1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[snakeIndex++];
+                        if (snakeIndex == snake.Length)
+                        {
+                            snakeIndex = 0;
+                        }
+                    }
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
