f72d77adb5389fb521b3093c4665595c4fa4fdb5
delev <delyan1@mail.bg>
2019-10-02 11:08:40 +0300

    Add MatrixShuffling solution

---
+++ C# Advanced/MultidimensionalArrays/MultidimensionalArraysEXS/MatrixShuffling/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixDimensions = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var rows = matrixDimensions[0];
+            var cols = matrixDimensions[1];
+
+            var matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                for (int col = 0; col < input.Length; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            var command = "";
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var commandArgs =command.Split().ToArray();
+
+                if (commandArgs[0] == "swap" && commandArgs.Length == 5)
+                {
+                    var row1 = int.Parse(commandArgs[1]);
+                    var col1 = int.Parse(commandArgs[2]);
+                    var row2 = int.Parse(commandArgs[3]);
+                    var col2 = int.Parse(commandArgs[4]);
+
+                    if (row1 >= 0 && row1 < matrix.GetLength(0) &&
+                        col1 >= 0 && col1 < matrix.GetLength(1) &&
+                        row2 >= 0 && row2 < matrix.GetLength(0) &&
+                        col2 >= 0 && col2 < matrix.GetLength(1))
+                    {
+                        var remainder = matrix[row1, col1];
+                        matrix[row1, col1] = matrix[row2, col2];
+                        matrix[row2, col2] = remainder;
+
+                        for (int row = 0; row < matrix.GetLength(0); row++)
+                        {
+                            for (int col = 0; col < matrix.GetLength(1); col++)
+                            {
+                                Console.Write(matrix[row,col]+ " ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+        }
+    }
+}
