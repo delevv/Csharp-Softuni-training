e1c81e4663c51341677655f993a0d9b0557d337b
delev <delyan1@mail.bg>
2019-10-03 17:06:08 +0300

    Add AverageStudentGrades solution

---
+++ C# Advanced/SetsAndDictionaries/SetsAndDictionariesLAB/AverageStudentGrades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentsCount = int.Parse(Console.ReadLine());
+               
+            var students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                var studentInfo = Console.ReadLine().Split();
+
+                var name = studentInfo[0];
+                var grade = double.Parse(studentInfo[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students[name] = new List<double>();
+                }
+                students[name].Add(grade);
+            }
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Ivancho -> {string.Join(" ", student.Value.Select(x => x.ToString("F2")))} ({student.Value.Average():f2})");
+            }
+        }
+    }
+}
