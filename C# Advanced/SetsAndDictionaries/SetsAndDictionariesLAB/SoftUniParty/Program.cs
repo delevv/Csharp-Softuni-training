04a588a9fbf059187afff90d03fcbb642971e3e3
delev <delyan1@mail.bg>
2019-10-03 18:24:19 +0300

    Add SoftUniParty solution

---
+++ C# Advanced/SetsAndDictionaries/SetsAndDictionariesLAB/SoftUniParty/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vipPartyList = new HashSet<string>();
+            var normalPartyList = new HashSet<string>();
+            var command = "";
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "PARTY")
+                {
+                    while((command = Console.ReadLine()) != "END")
+                    {
+                        if (char.IsDigit(command[0]))
+                        {
+                            vipPartyList.Remove(command);
+                        }
+                        else
+                        {
+                            normalPartyList.Remove(command)
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    if (char.IsDigit(command[0]))
+                    {
+                        vipPartyList.Add(command);
+                    }
+                    else
+                    {
+                        normalPartyList.Add(command)
+                    }
+                }
+            }
+            Console.WriteLine(vipPartyList.Count);
+
+            foreach (var name in vipPartyList)
+            {
+                Console.WriteLine(name);
+            }
+            foreach (var name in normalPartyList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
