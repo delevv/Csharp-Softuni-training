a49056e3709b829d84a19060974b7d1c9b8e1b2c
delev <delyan1@mail.bg>
2019-10-03 18:06:29 +0300

    Add RecordUniqueNames solution

---
+++ C# Advanced/SetsAndDictionaries/SetsAndDictionariesLAB/RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countOfNames = int.Parse(Console.ReadLine());
+
+            var names = new HashSet<string>();
+
+            for (int i = 0; i < countOfNames; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
