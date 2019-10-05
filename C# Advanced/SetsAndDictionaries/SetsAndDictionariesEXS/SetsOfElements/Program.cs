555d374708b0ec19c22a78a4ed50b35c5603eb33
delev <delyan1@mail.bg>
2019-10-05 17:01:53 +0300

    Add SetsOfElements solution

---
+++ C# Advanced/SetsAndDictionaries/SetsAndDictionariesEXS/SetsOfElements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setsCount = Console.ReadLine().Split(" ");
+
+            var firstSet = int.Parse(setsCount[0]);
+            var secondSet = int.Parse(setsCount[1]);
+
+            var firstNumbers = new HashSet<int>();
+            var secondNumbers = new HashSet<int>();
+
+            for (int i = 0; i < firstSet+secondSet; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                if (i < firstSet)
+                {
+                    firstNumbers.Add(currentNumber);
+                }
+                else
+                {
+                    secondNumbers.Add(currentNumber);
+                }
+            }
+            Console.WriteLine(string.Join(" ",firstNumbers.Intersect(secondNumbers)));
+        }
+    }
+}
