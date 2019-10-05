fb85ecb7df6c8aeb8b9dd58cf8f5fcf895ca0dc1
delev <delyan1@mail.bg>
2019-10-05 16:52:39 +0300

    Add UniqueUsernames solution

---
+++ C# Advanced/SetsAndDictionaries/SetsAndDictionariesEXS/UniqueUsernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countOfUsernames = int.Parse(Console.ReadLine());
+
+            var users = new HashSet<string>();
+
+            for (int i = 0; i < countOfUsernames; i++)
+            {
+                users.Add(Console.ReadLine());
+            }
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
