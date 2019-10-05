59e5bee5f3dd1bc985748a43fa4de7eec690244a
delev <delyan1@mail.bg>
2019-10-05 17:06:20 +0300

    Add PeriodicTable solution

---
+++ C# Advanced/SetsAndDictionaries/SetsAndDictionariesEXS/PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countOfElements = int.Parse(Console.ReadLine());
+
+            var elements = new SortedSet<string>();
+
+            for (int i = 0; i < countOfElements; i++)
+            {
+                var currentElements = Console.ReadLine().Split(" ");
+
+                foreach (var element in currentElements)
+                {
+                    elements.Add(element);
+                }
+            }
+            Console.WriteLine(string.Join(" ",elements));
+        }
+    }
+}
