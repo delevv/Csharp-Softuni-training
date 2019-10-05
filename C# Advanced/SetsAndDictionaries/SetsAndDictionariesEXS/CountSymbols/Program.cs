8195a1830545cfc05ef89a68fc7ac8409c8eb792
delev <delyan1@mail.bg>
2019-10-05 17:23:57 +0300

    Add CountSymbols solution

---
+++ C# Advanced/SetsAndDictionaries/SetsAndDictionariesEXS/CountSymbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+
+            var symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var currentSymbol = text[i];
+
+                if (!symbols.ContainsKey(currentSymbol))
+                {
+                    symbols[currentSymbol] = 1;
+                }
+                else
+                {
+                    symbols[currentSymbol]++;
+                }
+            }
+            foreach (var (symbol,symbolCount) in symbols)
+            {
+                Console.WriteLine($"{symbol}: {symbolCount} time/s");
+            }
+        }
+    }
+}
