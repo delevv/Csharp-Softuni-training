b4acd6434bb527b876f2dbab21b6a65099e7941c
delev <delyan1@mail.bg>
2019-10-05 17:18:48 +0300

    Add EvenTimes solution

---
+++ C# Advanced/SetsAndDictionaries/SetsAndDictionariesEXS/EvenTimes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countOfNumbers = int.Parse(Console.ReadLine());
+
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(currentNumber))
+                {
+                    numbers[currentNumber] = 1;
+                }
+                else
+                {
+                    numbers[currentNumber]++;
+                }
+            }
+            foreach (var kvp in numbers)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+            }
+        }
+    }
+}
