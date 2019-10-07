fee94dfddb74039c87b5dd07e4f719da75f16f1c
delev <delyan1@mail.bg>
2019-10-07 18:11:04 +0300

    Add WordCount solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesLAB/WordCount/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var readWords = new StreamReader(@"..\..\..\words.txt"))
+            using (var readText = new StreamReader(@"..\..\..\text.txt"))
+            using (var writer = new StreamWriter(@"..\..\..\output.txt"))
+            {
+                var words = new List<string>();
+
+                while (!readWords.EndOfStream)
+                {
+                    words.AddRange(readWords.ReadLine().ToLower().Split());
+                }
+
+                var textLines = new List<string>();
+
+                while (!readText.EndOfStream)
+                {
+                    textLines.Add(readText.ReadLine().ToLower());
+                }
+
+                var matchedWords = new Dictionary<string, int>();
+
+                foreach (var line in textLines)
+                {
+                    foreach (var word in words)
+                    {
+                        if (line.Contains(word))
+                        {
+                            if (words.Contains(word))
+                            {
+                                if (!matchedWords.ContainsKey(word))
+                                {
+                                    matchedWords[word] = 0;
+                                }
+                                matchedWords[word]++;
+                            }
+                        }
+                    }
+                }
+                foreach (var kvp in matchedWords.OrderByDescending(x=>x.Value))
+                {
+                    writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+                }
+            }
+        }
+    }
+}
