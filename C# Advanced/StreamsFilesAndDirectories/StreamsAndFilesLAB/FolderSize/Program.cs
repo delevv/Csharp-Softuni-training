47f839a8fb21c7649a2c45a1af682726e2bfa33e
delev <delyan1@mail.bg>
2019-10-07 18:59:55 +0300

    Add FolderSize solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesLAB/FolderSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(@"..\..\..\TestFolder");
+            var sum = 0.0;
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+            sum = sum / 1024 / 1024;
+
+            File.WriteAllText(@"..\..\..\output.txt", sum.ToString());
+        }
+    }
+}
