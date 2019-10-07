856fd53ad0804297339fa0750b6b15ea7e19af98
delev <delyan1@mail.bg>
2019-10-07 17:10:24 +0300

    Add OddLines solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesLAB/OddLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var writer=new StreamWriter(@"..\..\..\output.txt"))
+            using (var reader = new StreamReader(@"..\..\..\input.txt"))
+            {
+                var lineCounter = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    var currentLine = reader.ReadLine();
+
+                    if (lineCounter % 2 == 1)
+                    {
+                        writer.WriteLine(currentLine);
+                    }
+                    lineCounter++;
+                }
+            }
+        }
+    }
+}
