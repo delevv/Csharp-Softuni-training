148e0fb2706536e27b44b2eb0a9b37985b1600ca
delev <delyan1@mail.bg>
2019-10-07 18:21:28 +0300

    Add MergeFiles solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesLAB/MergeFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"..\..\..\FileOne.txt"))
+            using (var secondReader = new StreamReader(@"..\..\..\FileTwo.txt"))
+            using (var writer = new StreamWriter(@"..\..\..\output.txt"))
+            {
+                while (!reader.EndOfStream && !secondReader.EndOfStream)
+                {
+                    var currentLine = reader.ReadLine();
+                    writer.WriteLine(currentLine);
+
+                    currentLine = secondReader.ReadLine();
+                    writer.WriteLine(currentLine);
+                }
+            }
+        }
+    }
+}
