2d4bab53889afcf843c76f63c358ebbd0ce2b02d
delev <delyan1@mail.bg>
2019-10-07 18:51:43 +0300

    Add SliceAFile solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesLAB/SliceAFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace SliceAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var parts = 4;
+            var totalSize = new FileInfo(@"..\..\..\sliceMe.txt").Length;
+            var partSize = (int)totalSize / parts;
+
+            using (var read = new FileStream(@"..\..\..\sliceMe.txt", FileMode.Open))
+            {
+                for (int i = 1; i <= parts; i++)
+                {
+                    if (i == parts)
+                    {
+                        partSize = (int)(totalSize - (partSize * (parts - 1)));
+                    }
+                    var buffer = new byte[partSize];                   
+
+                    read.Read(buffer, 0,partSize);
+
+                    using (var writer = new FileStream($@"..\..\..\Part-{i}.txt", FileMode.OpenOrCreate))
+                    {
+                        writer.Write(buffer, 0,partSize);
+                    }
+                }
+            }
+        }
+    }
+}
