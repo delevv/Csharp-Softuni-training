be3a8404e61035b7fb8414ae02676709a62069aa
delev <delyan1@mail.bg>
2019-10-07 17:24:59 +0300

    Add LineNumbers solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesLAB/LineNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"..\..\..\Input.txt"))
+            using (var writer = new StreamWriter(@"..\..\..\output.txt"))
+            {
+                var lineNumber = 1;
+
+                while (!reader.EndOfStream)
+                {
+                    var currentLine = reader.ReadLine();
+                    writer.WriteLine($"{lineNumber++}. {currentLine}");
+                }
+            }
+        }
+    }
+}
