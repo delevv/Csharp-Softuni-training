b9046e4b5b5b675220f7355629ceb350d397a078
delev <delyan1@mail.bg>
2019-10-08 11:52:36 +0300

    Add EvenLines solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesEXS/EvenLines/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var symbols = new char[] { '-', ',', '.', '!', '?' };
+
+            using (var reader = new StreamReader(@"..\..\..\text.txt"))
+            {
+                var counter = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    var currentLine = reader.ReadLine().Split().ToList();
+
+                    if (counter % 2 == 0)
+                    {
+                        for (int i = 0; i < currentLine.Count; i++)
+                        {
+                            var currentWord = currentLine[i];
+                            foreach (var symbol in symbols)
+                            {
+                                currentWord = currentWord.Replace(symbol, '@');
+                                currentLine[i] = currentWord;
+
+
+                            }
+                        }
+                        currentLine.Reverse();
+                        Console.WriteLine(string.Join(" ", currentLine));
+                    }
+                    counter++;
+                }
+            }
+        }
+    }
+}
