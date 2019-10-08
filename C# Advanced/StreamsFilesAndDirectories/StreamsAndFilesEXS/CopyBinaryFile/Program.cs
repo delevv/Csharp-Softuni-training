c589f0b934865707795436482e4f721693386e89
delev <delyan1@mail.bg>
2019-10-08 16:54:33 +0300

    Add CopyBinaryFile solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesEXS/CopyBinaryFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new FileStream(@"..\..\..\copyMe.png", FileMode.Open))
+            {
+                using (var writer = new FileStream(@"..\..\..\copyResult.png", FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+
+                    var readBytes = 0;
+
+                    while ((readBytes = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        writer.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+        }
+    }
+}
