234f0b31efe80285d3a5a0fb8c5ac308388e4af5
delev <delyan1@mail.bg>
2019-10-08 12:23:26 +0300

    Add LineNumbers solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesEXS/LineNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"..\..\..\text.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var currentLine = lines[i];
+
+                var lettersCount = 0;
+                var punctuationCount = 0;
+
+                foreach (var symbol in currentLine)
+                {
+                    if (char.IsLetter(symbol))
+                    {
+                        lettersCount++;
+                    }
+                    else if (symbol != ' ')
+                    {
+                        punctuationCount++;
+                    }
+                }
+                File.AppendAllText(@"..\..\..\output.txt",$"Line {i + 1}: {currentLine} ({lettersCount})({punctuationCount})"+Environment.NewLine);
+            }
+        }
+    }
+}
