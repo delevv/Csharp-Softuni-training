4a2edd207efac274f02500b3c15b5583261932ad
delev <delyan1@mail.bg>
2019-10-09 16:48:25 +0300

    Add CountUppercaseWords solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingLAB/CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                  .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                  .Where(x => char.IsUpper(x[0]));
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
