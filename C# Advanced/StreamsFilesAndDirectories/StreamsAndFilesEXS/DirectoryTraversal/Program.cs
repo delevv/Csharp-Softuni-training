731cf50d6c5fdc4b04432f3ced7f08911b652db5
delev <delyan1@mail.bg>
2019-10-09 10:16:07 +0300

    Finish StreamsAndFilesEXS solution

---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesEXS/DirectoryTraversal/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = new DirectoryInfo(Environment.CurrentDirectory);
+            var files = directory.GetFiles();
+
+            var filesByExtension = new Dictionary<string, Dictionary<string, long>>();
+
+            foreach (var file in files)
+            {
+                var extension = file.Extension;
+
+                if (!filesByExtension.ContainsKey(extension))
+                {
+                    filesByExtension[extension] = new Dictionary<string, long>();
+                }
+
+                filesByExtension[extension][file.Name] = file.Length;
+            }
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            using (var writer = new StreamWriter($"{desktopPath}" + "/report.txt"))
+            {
+                foreach (var extension in filesByExtension
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value))
+                {
+                    writer.WriteLine(extension.Key);
+                    
+                    foreach (var file in extension.Value
+                        .OrderBy(x => x.Value)
+                        .ToDictionary(x => x.Key, x => x.Value))
+                    {
+                        writer.WriteLine($"--{file.Key} - {file.Value / 1000.0:F3}kb");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/StreamsFilesAndDirectories/StreamsAndFilesEXS/ZipAndExtract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zipPath = $"../../../resultZip.zip";
+            var filesToZip = $"../../../copyMe.png";
+
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            using (var zip = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+            {
+                zip.CreateEntryFromFile(filesToZip, "copyMe.png");
+            }
+
+            ZipFile.ExtractToDirectory(zipPath, desktopPath);
+
+        }
+    }
+}
