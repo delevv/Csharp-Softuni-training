8ddc2f5fda0676e295c8a645d168a4fea1683f37
delev <delyan1@mail.bg>
2019-10-09 16:34:28 +0300

    Add SumNumbers solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingLAB/SumNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                 .Split(", ")
+                 .Select(int.Parse);
+
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());       
+        }
+    }
+}
