0ad3b57e5293fb78e1aef0c8e91d38f9bcc2b1f0
delev <delyan1@mail.bg>
2019-10-09 17:27:44 +0300

    Add FilterByAge solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingLAB/FilterByAge/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilterByAge
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countOfPersons = int.Parse(Console.ReadLine());
+
+            var persons = new List<Person>();
+
+            for (int i = 0; i < countOfPersons; i++)
+            {
+                var currentPersonInfo = Console.ReadLine().Split(", ");
+
+                var currentPerson = new Person();
+
+                currentPerson.Name = currentPersonInfo[0];
+                currentPerson.Age = int.Parse(currentPersonInfo[1]);
+
+                persons.Add(currentPerson);
+            }
+            var conditionType = Console.ReadLine();
+            var conditionNumber = int.Parse(Console.ReadLine());
+
+            Func<Person, bool> condition;
+
+            if (conditionType == "older")
+            {
+                condition = p => p.Age >= conditionNumber;
+            }
+            else
+            {
+                condition = p => p.Age <= conditionNumber;
+            }
+
+            var personsAfterFilter = persons.Where(condition);
+
+            var format = Console.ReadLine();
+
+            foreach (var person in personsAfterFilter)
+            {
+                switch (format)
+                {
+                    case "name age":
+                        Console.WriteLine($"{person.Name} - {person.Age}");
+                        break;
+                    case "name":
+                        Console.WriteLine($"{person.Name}");
+                        break;
+                    case "age":
+                        Console.WriteLine($"{person.Age}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
