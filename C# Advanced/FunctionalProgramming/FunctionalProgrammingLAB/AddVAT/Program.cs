c8da07f7f7536b692386e3e645d69fb3a8377d9c
delev <delyan1@mail.bg>
2019-10-09 16:54:49 +0300

    Add AddVAT solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingLAB/AddVAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(x => x * 1.20);
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
