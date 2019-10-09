bc1cc203f6ece5e9a0a08ef8135b9ad98278bea4
delev <delyan1@mail.bg>
2019-10-09 16:32:35 +0300

    Add SortEvenNumbers solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingLAB/SortEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                 .Split(", ")
+                 .Select(int.Parse)
+                 .Where(x => x % 2 == 0)
+                 .OrderBy(x => x);
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
