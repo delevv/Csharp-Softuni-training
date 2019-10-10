e7b22ae70fe031dbcb74a2070fe514f2d50eeda8
delev <delyan1@mail.bg>
2019-10-10 16:06:14 +0300

    Add ActionPoint solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/ActionPoint/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = name => Console.WriteLine(name);
+
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(x => print(x));
+        }
+    }
+}
