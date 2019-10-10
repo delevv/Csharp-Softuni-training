842a679e24423596e73dd76ef8d619a4e0c212ff
delev <delyan1@mail.bg>
2019-10-10 17:32:04 +0300

    Add CustomComparator solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/CustomComparator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> CustomComparer = (a, b) =>
+              {
+                  if(a%2==0 && b % 2 == 0)
+                  {
+                      if (a < b)
+                      {
+                          return -1;
+                      }
+                      else if (a > b)
+                      {
+                          return 1;
+                      }
+                      return 0;
+                  }
+                  if (a % 2 != 0 && b % 2 != 0)
+                  {
+                      if (a < b)
+                      {
+                          return -1;
+                      }
+                      else if (a > b)
+                      {
+                          return 1;
+                      }
+                      return 0;
+                  }
+                  if (a % 2 == 0)
+                  {
+                      return -1;
+                  }
+                  if (b % 2 == 0)
+                  {
+                      return 1;
+                  }
+                  return 0;
+              };
+
+            Array.Sort(numbers, new Comparison<int>(CustomComparer));
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
