a893bddc68ad67868ede3044fcd38d774f39137b
delev <delyan1@mail.bg>
2019-10-10 16:08:29 +0300

    Add KnightsOfHonor solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = name => Console.WriteLine("Sir " + name);
+
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(x => print(x));
+        }
+    }
+}
