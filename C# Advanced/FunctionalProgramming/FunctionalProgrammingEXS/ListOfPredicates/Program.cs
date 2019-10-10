7f09423269db69a2eacc7cff350c510c264b3f40
delev <delyan1@mail.bg>
2019-10-10 17:46:43 +0300

    Add ListOfPredicates solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/ListOfPredicates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxNumber = int.Parse(Console.ReadLine());
+            var divideNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var numbers = new List<int>();
+
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Func<int, int[], bool> divideFunc = (num, divideNums) =>
+            {
+                   bool isDivide = true;
+
+                   foreach (var divideNum in divideNums)
+                   {
+                       if (num % divideNum != 0)
+                       {
+                           isDivide = false;
+                       }
+                   }
+                   return isDivide;
+            };
+
+            numbers = numbers
+                .Where(x => divideFunc(x, divideNumbers))
+                .ToList();
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
