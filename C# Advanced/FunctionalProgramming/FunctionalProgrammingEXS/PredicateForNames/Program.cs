4963b54613e09ee12a6ef74d8d46202591b3e33b
delev <delyan1@mail.bg>
2019-10-10 17:14:56 +0300

    Add PredicateForNames solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/PredicateForNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameLenght = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine().Split();
+
+            Func<string, bool> nameFunc = name => name.Length <= nameLenght;
+
+            names
+                .Where(nameFunc)
+                .ToList()
+                .ForEach(name=>Console.WriteLine(name));    
+        }
+    }
+}
