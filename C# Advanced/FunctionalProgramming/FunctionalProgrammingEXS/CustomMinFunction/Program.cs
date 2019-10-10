ddb4715ba179ffe7d9205f9f411d2d361a598d96
delev <delyan1@mail.bg>
2019-10-10 16:18:08 +0300

    Add CustomMinFunction solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/CustomMinFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minFunc = numbers =>
+            {
+                var minNumber = int.MaxValue;
+
+                foreach (var number in numbers)
+                {
+                    if (number < minNumber)
+                    {
+                        minNumber = number;
+                    }
+                }
+                return minNumber;
+            };
+
+            var inputNumbers= Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(minFunc(inputNumbers)); 
+        }
+    }
+}
