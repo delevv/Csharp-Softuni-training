e11eb702500065c7a2ebd28e44251a1c26ec4f0c
delev <delyan1@mail.bg>
2019-10-10 16:58:12 +0300

    Add AppliedArithmetics solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/AppliedArithmetics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Func<int, int> addFunc = num => num += 1;
+            Func<int, int> multiplyFunc = num => num *= 2;
+            Func<int, int> suntractFunc = num => num -= 1;
+
+            var command = "";
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "add")
+                {
+                    numbers = numbers.Select(addFunc).ToList();
+                }
+                else if (command == "multiply")
+                {
+                    numbers = numbers.Select(multiplyFunc).ToList();
+
+                }
+                else if (command == "subtract")
+                {
+                    numbers = numbers.Select(suntractFunc).ToList();
+
+                }
+                else if (command == "print")
+                {
+                    Console.WriteLine(string.Join(" ",numbers));
+                }
+            }
+        }
+    }
+}
