57e11164c29abf6b7d12c2c5a584a12bbcae1bc7
delev <delyan1@mail.bg>
2019-10-10 17:04:43 +0300

    Add ReverseAndExclude solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/ReverseAndExclude/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            var divideNumber = int.Parse(Console.ReadLine());
+
+            Func<int, bool> divideFunc = num => num % divideNumber == 0;
+
+            numbers = numbers
+                .Where(x => !divideFunc(x))
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
