ab7b9779ee62a6a9279003f3a61f24f1cd4aca50
delev <delyan1@mail.bg>
2019-10-11 10:49:43 +0300

    Add TriFunction solution

---
+++ C# Advanced/FunctionalProgramming/FunctionalProgrammingEXS/TriFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> isEqualOrLargerFunc = (word, param) => word.Sum(x => x) >= param;
+
+            var targetSum = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine().Split();
+
+            Func<string[], Func<string, int, bool>,int, string> filterFunc = (peopleNames, isLargerFunc,neededSum) =>
+            {
+                return peopleNames.FirstOrDefault(x => isLargerFunc(x, neededSum));
+            };
+
+            var resultName = filterFunc(names, isEqualOrLargerFunc,targetSum);
+
+            Console.WriteLine(resultName);
+        }
+    }
+}
