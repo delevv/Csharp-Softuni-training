96155f4afc1a7e4171b5bc656f9363ad6873bcae
delev <delyan1@mail.bg>
2019-10-13 14:08:42 +0300

    Add Person class

---
+++ C# Advanced/DefiningClasses/DefiningClassesEXS/DefiningClasses/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person();
+
+            var name = "Pesho";
+            var age = 20;
+
+            person.Name = name;
+            person.Age = age;
+        }   
+    }
+}
