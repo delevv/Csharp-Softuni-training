30a966c141091aa8560ac93b6eee4a9dac5417fc
delev <delyan1@mail.bg>
2019-10-13 15:28:34 +0300

    Add DateModifier solution

---
+++ C# Advanced/DefiningClasses/DefiningClassesEXS/DateModifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public double GetDifferenceInDaysBetweenTwoDates(string firstDate,string secondDate)
+        {
+            var startDate = DateTime.Parse(firstDate);
+            var endDate = DateTime.Parse(secondDate);
+
+            return Math.Abs((startDate - endDate).TotalDays);
+        }
+    }
+}
---
+++ C# Advanced/DefiningClasses/DefiningClassesEXS/DateModifier/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            var dateModifier = new DateModifier();
+
+            double result = dateModifier.GetDifferenceInDaysBetweenTwoDates(firstDate, secondDate);
+            Console.WriteLine(result);
+        }
+    }
+}
