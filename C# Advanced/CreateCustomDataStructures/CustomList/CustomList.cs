163af0e457583b95dd8d37855f2a831cb6976c02
delev <delyan1@mail.bg>
2019-10-16 10:44:13 +0300

    Create CustomList

---
+++ C# Advanced/CreateCustomDataStructures/CustomList/CustomList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+
+        private int[] items;
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= this.Count || index < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return this.items[index];
+            }
+            set
+            {
+                if (index >= this.Count || index < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                this.items[index] = value;
+            }
+        }
+
+
+
+        public CustomList()
+        {
+            this.items = new int[InitialCapacity];
+            this.Count = 0;
+        }
+
+
+
+        public void Add(int element)
+        {
+
+        }
+        public int RemoveAt(int index)
+        {
+            return 0;
+        }
+        public bool Contains(int element)
+        {
+            return true;
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+
+        }
+
+        private void Resize()
+        {
+
+        }
+        private void Shrink()
+        {
+
+        }
+        private void Shift()
+        {
+
+        }
+    }
+}
---
+++ C# Advanced/CreateCustomDataStructures/CustomList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
