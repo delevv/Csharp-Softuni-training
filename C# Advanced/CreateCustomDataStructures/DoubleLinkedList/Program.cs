3ba6a1119f9acf4fdf3e1625f545a7a6b14ade0e
delev <delyan1@mail.bg>
2019-10-16 17:59:43 +0300

    Add DoubleLinkedList class

---
+++ C# Advanced/CreateCustomDataStructures/DoubleLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DoubleLinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //•	void AddFirst(int element) – adds an element at the beginning of the collection
+            //•	void AddLast(int element) – adds an element at the end of the collection
+            //•	int RemoveFirst() – removes the element at the beginning of the collection
+            //•	int RemoveLast() – removes the element at the end of the collection
+            //•	void ForEach() – goes through the collection and executes a given action
+            //•	int[] ToArray() – returns the collection as an array
+        }
+    }
+}
