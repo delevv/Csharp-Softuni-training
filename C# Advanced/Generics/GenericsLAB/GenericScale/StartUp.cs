857d371a194c1f0f5da2f6b2703fbc25ac3c0357
delev <delyan1@mail.bg>
2019-10-18 12:51:19 +0300

    Add EqualityScale solution

---
+++ C# Advanced/Generics/GenericsLAB/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var equal = new EqualityScale<string>("Gosho", "Gosho");
+
+            Console.WriteLine(equal.AreEqual());
+        }
+    }
+}
