fc44376ee330f5ab259a2d3d8475f6fa3e1ccc1b
delev <delyan1@mail.bg>
2019-10-18 14:12:50 +0300

    Add GenericCountMethodStrings

---
+++ C# Advanced/Generics/GenericsEXS/GenericCountMethodStrings/Box.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box()
+        {
+            this.Values = new List<T>();
+        }
+
+
+        public List<T> Values { get; set; }
+
+        public int CountOfGreaterValuesThan(T targetItem)
+        {
+            int counter = 0;
+
+            foreach (var value in this.Values)
+            {
+                int isGreater = value.CompareTo(targetItem);
+
+                if (isGreater == 1)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var value in Values)
+            {
+                sb.AppendLine($"{value.GetType()}: {value}");
+            }
+            return sb.ToString().Trim();
+        }
+
+        void CheckIndex(int a)
+        {
+            bool isInRange = a >= 0 && a < Values.Count;
+
+            if (!isInRange)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
+
---
+++ C# Advanced/Generics/GenericsEXS/GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var counter = int.Parse(Console.ReadLine());
+
+            var box = new Box<string>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.Values.Add(input);
+            }
+
+            var targetItem = Console.ReadLine();
+
+           int result= box.CountOfGreaterValuesThan(targetItem);
+
+            Console.WriteLine(result);
+        }
+    }
+}
