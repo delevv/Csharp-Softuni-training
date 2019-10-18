774deba4ea7f6a9654ea457733373fe9dd01026f
delev <delyan1@mail.bg>
2019-10-18 13:06:07 +0300

    Add GenericBoxOfString solution

---
+++ C# Advanced/Generics/GenericsEXS/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                string input = Console.ReadLine();
+
+                var box = new Box<string>(input);
+
+                Console.WriteLine(box);
+            }
+           
+        }
+    }
+}
