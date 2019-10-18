a1a3fbefde0e969685bb2b1f0e68832c4beb73e1
delev <delyan1@mail.bg>
2019-10-18 14:49:33 +0300

    Add Tuple solution

---
+++ C# Advanced/Generics/GenericsEXS/Tuple/MyTuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class MyTuple<Titem1,Titem2>
+    {
+        public MyTuple(Titem1 item1, Titem2 item2)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+        }
+
+        public Titem1 item1 { get; set; }
+        public Titem2 item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2}";
+        }
+    }
+}
---
+++ C# Advanced/Generics/GenericsEXS/Tuple/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tuple
+{
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var inputPersonInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var inputPersonBeer = Console.ReadLine()
+                .Split();
+            var inputNumbersInfo = Console.ReadLine()
+                .Split();
+
+            var fullName = inputPersonInfo[0] + " " + inputPersonInfo[1];
+            var address = inputPersonInfo[2];
+
+            var name = inputPersonBeer[0];
+            var liters = int.Parse(inputPersonBeer[1]);
+
+            var myInt = int.Parse(inputNumbersInfo[0]);
+            var myDouble = double.Parse(inputNumbersInfo[1]);
+
+            var personInfo = new MyTuple<string, string>(fullName, address);
+            var personBeer = new MyTuple<string, int>(name, liters);
+            var numbersInfo = new MyTuple<int, double>(myInt, myDouble);
+
+            Console.WriteLine(personInfo);
+            Console.WriteLine(personBeer);
+            Console.WriteLine(numbersInfo);
+        }
+    }
+}
