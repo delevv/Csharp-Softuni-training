1ceb0e0e1a01e4f01b38b6e332176c7ee19c2b14
delev <delyan1@mail.bg>
2019-10-18 13:28:33 +0300

    Add GenericSwapMethodStrings solution

---
+++ C# Advanced/Generics/GenericsEXS/GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            this.Values = new List<T>();
+        }
+
+        
+        public List<T> Values { get; set; }
+
+        public void Swap(int a,int b)
+        {
+            CheckIndex(a);
+            CheckIndex(b);
+
+            var temp = this.Values[a];
+
+            Values[a] = Values[b];
+            Values[b] = temp;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var value in Values)
+            {
+                sb.AppendLine($"{value.GetType()}: {value}");
+            }
+            return sb.ToString().Trim() ;
+        }
+
+        void CheckIndex(int a)
+        {
+            bool isInRange = a >= 0 && a < Values.Count;
+
+            if (!isInRange)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ C# Advanced/Generics/GenericsEXS/GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var counter = int.Parse(Console.ReadLine());
+
+            var box = new Box<string>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                var input = Console.ReadLine();
+
+                box.Values.Add(input);
+            }
+
+            var swapIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var firstIndex = swapIndexes[0];
+            var secondIndex = swapIndexes[1];
+
+            box.Swap(firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+    }
+}
