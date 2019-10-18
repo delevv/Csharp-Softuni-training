dc9cc3c7106cfc31c5c641051ff44e833c7624ef
delev <delyan1@mail.bg>
2019-10-18 12:33:33 +0300

    Add boxOfT solution

---
+++ C# Advanced/Generics/GenericsLAB/BoxOfT/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+       private List<T> List { get; set; }
+
+        public int Count => this.List.Count;
+
+        public Box()
+        {
+            this.List = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.List.Add(element);
+        }
+        public T Remove()
+        {
+            var elementToRemove = List[List.Count - 1];
+            List.RemoveAt(List.Count - 1);
+
+            return elementToRemove;
+        }
+    }
+}
