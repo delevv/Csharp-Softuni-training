70095dbb8b4931240efed940846c84a68691619d
delev <delyan1@mail.bg>
2019-10-21 11:34:33 +0300

    Add Repository solution

---
+++ C# Advanced/ExamPreparation/CSharpAdvancedExam17Feb2019/Repository/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class Person
+    {
+        public Person(string name, int age, DateTime bithdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = bithdate;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
---
+++ C# Advanced/ExamPreparation/CSharpAdvancedExam17Feb2019/Repository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class Repository
+    {
+        private Dictionary<int,Person> data;
+        public int Count => this.data.Count;
+
+        public Repository()
+        {
+            this.data = new Dictionary<int, Person>();
+        }
+
+        public void Add(Person person)
+        {
+            this.data[this.data.Count] = person;
+        }
+
+        public Person Get(int id)
+        {
+            return this.data[id];
+        }
+
+        public bool Update(int id, Person newPerson)
+        {
+            if(this.data.ContainsKey(id))
+            {
+                this.data[id] = newPerson;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            if (this.data.ContainsKey(id))
+            {
+                this.data.Remove(id);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
