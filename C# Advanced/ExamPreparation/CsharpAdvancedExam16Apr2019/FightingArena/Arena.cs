9fce64418f435aa47e8b1e7de00a8d1af49b3205
delev <delyan1@mail.bg>
2019-10-22 12:31:16 +0300

    Add Arena solution

---
+++ C# Advanced/ExamPreparation/CsharpAdvancedExam16Apr2019/FightingArena/Arena.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FightingArena
+{
+    public class Arena
+    {
+        private List<Gladiator> gladiators;
+        public string Name { get; set; }
+        public int Count => this.gladiators.Count;
+
+        public Arena(string name)
+        {
+            this.gladiators = new List<Gladiator>();
+            this.Name = name;
+        }
+
+        public void Add(Gladiator gladiator)
+        {
+            this.gladiators.Add(gladiator);
+        }
+
+        public void Remove(string name)
+        {
+            var gladiatorToRemove = gladiators.Where(g => g.Name == name).FirstOrDefault();
+
+            this.gladiators.Remove(gladiatorToRemove);
+        }
+
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            return this.gladiators.OrderByDescending(g => g.GetStatPower()).FirstOrDefault();
+        }
+
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            return this.gladiators.OrderByDescending(g => g.GetWeaponPower()).FirstOrDefault();
+        }
+
+        public  Gladiator GetGladitorWithHighestTotalPower()
+        {
+            return this.gladiators.OrderByDescending(g => g.GetTotalPower()).FirstOrDefault();
+        }
+
+        public override string ToString()
+        {
+            return $"[{this.Name}] - [{this.Count}] gladiators are participating.";
+        }
+    }
+}
---
+++ C# Advanced/ExamPreparation/CsharpAdvancedExam16Apr2019/FightingArena/Gladiator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FightingArena
+{
+    public class Gladiator
+    {
+        public Gladiator(string name, Stat stat, Weapon weapon)
+        {
+            this.Name = name;
+            this.Stat = stat;
+            this.Weapon = weapon;
+        }
+
+        public string Name { get; set; }
+        public Stat Stat { get; set; }
+        public Weapon Weapon { get; set; }
+
+        public int GetTotalPower()
+        {
+            return GetStatPower() + GetWeaponPower();
+        }
+
+        public int GetWeaponPower()
+        {
+            return this.Weapon.Sharpness +
+                this.Weapon.Size +
+                this.Weapon.Solidity;
+        }
+
+        public int GetStatPower()
+        {
+            return this.Stat.Agility +
+                this.Stat.Flexibility +
+                this.Stat.Intelligence +
+                this.Stat.Skills +
+                this.Stat.Strength;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"[{this.Name}] - [{GetTotalPower()}]");
+            sb.AppendLine($"  Weapon Power: [{GetWeaponPower()}]");
+            sb.AppendLine($"  Stat Power: [{GetStatPower()}]");
+
+            return sb.ToString().Trim(); 
+        }
+    }
+}
