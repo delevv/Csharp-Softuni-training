5bf6a9e0037dd5f23a130eff9d3213ffa03bc61d
delev <delyan1@mail.bg>
2019-10-28 16:10:26 +0200

    Add Froggy solution

---
+++ C# Advanced/IteratorsAndComparators/IteratorsComparatorsEXS/Froggy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+
+            var lake = new Lake(numbers);
+
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}
