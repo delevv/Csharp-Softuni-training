4146f43658776e1c1bcd915c48677ede70ba9ed8
delev <delyan1@mail.bg>
2019-10-28 15:24:52 +0200

    Add Stack solution

---
+++ C# Advanced/IteratorsAndComparators/IteratorsComparatorsEXS/Stack/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+
+            var firstCommand = Console.ReadLine().Replace("Push ","").Split(", ").Select(int.Parse).ToArray();
+
+
+            foreach (var item in firstCommand)
+            {
+                stack.Push(item);
+            }
+
+            var command = "";
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var commandArgs = command.Split();
+
+                if (commandArgs[0] == "Pop")
+                {
+                    if (stack.Count() == 0)
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if(commandArgs[0]=="Push")
+                {
+                    for (int i = 1; i < commandArgs.Length; i++)
+                    {
+                        stack.Push(int.Parse(commandArgs[i]));
+                    }
+                }
+            }
+
+            for (int i = 1; i <= 2; i++)
+            {
+                foreach (var item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/IteratorsComparatorsEXS/Stack/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    class Stack<T> : IEnumerable<T>
+    {
+        private List<T> list;
+
+        public Stack()
+        {
+            this.list = new List<T>();
+        }
+
+        public void Push(T element)
+        {
+            this.list.Add(element);   
+        }
+
+        public T Pop()
+        {
+            var currentElement = this.list[list.Count - 1];
+            this.list.RemoveAt(this.list.Count-1);
+            return currentElement;
+        }
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.list.Count - 1; i >= 0; i--)
+            {
+                yield return this.list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
