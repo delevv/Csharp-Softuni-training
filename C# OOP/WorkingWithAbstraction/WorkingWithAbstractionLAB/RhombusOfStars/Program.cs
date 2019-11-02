b56febc7499c6dfcdcfb08d895f17487e20e6fbd
delev <delyan1@mail.bg>
2019-11-02 17:06:28 +0200

    Add RhombusOfStars solution

---
+++ C# OOP/WorkingWithAbstraction/WorkingWithAbstractionLAB/RhombusOfStars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RhombusOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var figureSize = int.Parse(Console.ReadLine());
+
+            for (int starsCount = 1; starsCount <= figureSize; starsCount++)
+            {
+                PrintRow(figureSize, starsCount);
+                Console.WriteLine();
+            }
+         
+            for (int starsCount = figureSize-1; starsCount >= 1; starsCount--)
+            {
+                PrintRow(figureSize, starsCount);
+                Console.WriteLine();
+            }
+        }
+        static void PrintRow(int figureSize,int starsCount)
+        {
+            Console.Write(new string (' ',figureSize-starsCount));
+
+            for (int i = 0; i < starsCount; i++)
+            {
+                Console.Write("* ");
+            }
+        }
+    }
+}
