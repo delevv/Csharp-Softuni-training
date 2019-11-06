5e42043146f0f611f3de11acaa9f17b46c95e9ff
delev <delyan1@mail.bg>
2019-11-06 15:20:10 +0200

    Add CustomRandomList solution

---
+++ C# OOP/Inheritance/InheritanceLAB/CustomRandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            var randomIndex = new Random().Next(0,this.Count);
+
+            var currentString = this[randomIndex];
+
+            this.RemoveAt(randomIndex);
+
+            return currentString;
+        }
+    }
+}
---
+++ C# OOP/Inheritance/InheritanceLAB/CustomRandomList/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var list = new RandomList() { "1", "2", "3" };
+
+            //Console.WriteLine(list.RandomString());
+        }
+    }
+}
