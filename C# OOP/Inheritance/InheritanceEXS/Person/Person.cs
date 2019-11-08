58b8c4eedb9e8f2251a3f401b4e8b5f9e1219d3f
delev <delyan1@mail.bg>
2019-11-08 16:32:20 +0200

    Add Person solution

---
+++ C# OOP/Inheritance/InheritanceEXS/Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append($"Name: {this.Name}, Age: {this.Age}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/Inheritance/InheritanceEXS/Person/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person person=null;
+
+            if (age>=0 && age <= 15)
+            {
+                person = new Child(name, age);
+            }
+            else if (age >= 0)
+            {
+                person = new Person(name,age);
+            }
+             
+            Console.WriteLine(person);
+        }
+    }
+}
