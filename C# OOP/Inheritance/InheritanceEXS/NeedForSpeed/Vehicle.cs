a851b6c3f331dbd5edc24a0e862e7c2223301e12
delev <delyan1@mail.bg>
2019-11-08 17:43:03 +0200

    Add NeedForSpeed solution

---
+++ C# OOP/Inheritance/InheritanceEXS/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption= 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public virtual void Drive(double km)
+        {
+            var neededFuel = km * this.FuelConsumption;
+
+            if (this.Fuel >= neededFuel)
+            {
+                this.Fuel -= neededFuel;
+            }
+        }
+    }
+}
