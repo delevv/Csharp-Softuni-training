f1b8a3c936a57b82ff4776224406cbb34d709713
delev <delyan1@mail.bg>
2019-11-10 17:21:41 +0200

    Add Validation of Data solution

--- C# OOP/Encapsulation/EncapsulationLAB/PersonsInfo/Person.cs
+++ C# OOP/Encapsulation/EncapsulationLAB/PersonsInfo/Person.cs
@@ -1,8 +1,15 @@
-﻿namespace PersonsInfo
+﻿using System;
+
+namespace PersonsInfo
 {
     public class Person
     {
-        public Person(string firstName, string lastName, int age,decimal salary)
+        private string firstName;
+        private string lastName;
+        private decimal salary;
+        private int age;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             this.FirstName = firstName;
             this.LastName = lastName;
@@ -10,11 +17,58 @@
             this.Salary = salary;
         }
 
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
 
-        public decimal Salary{ get; private set; }
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                this.age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get => salary;
+            set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+
+                this.salary = value;
+            }
+        }
 
         public void IncreaseSalary(decimal percentage)
         {
@@ -25,9 +79,10 @@
 
             this.Salary *= 1 + (percentage / 100);
         }
+
         public override string ToString()
         {
-            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+            return $"{this.FirstName} {this.LastName} gets {this.Salary:f2} leva.";
         }
     }
 }
--- C# OOP/Encapsulation/EncapsulationLAB/PersonsInfo/StartUp.cs
+++ C# OOP/Encapsulation/EncapsulationLAB/PersonsInfo/StartUp.cs
@@ -9,20 +9,30 @@ namespace PersonsInfo
         {
             var lines = int.Parse(Console.ReadLine());
             var persons = new List<Person>();
+
+
             for (int i = 0; i < lines; i++)
             {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0],
-                                        cmdArgs[1],
-                                        int.Parse(cmdArgs[2]),
-                                        decimal.Parse(cmdArgs[3]));
+                try
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                                            cmdArgs[1],
+                                            int.Parse(cmdArgs[2]),
+                                            decimal.Parse(cmdArgs[3]));
 
-                persons.Add(person);
+                    persons.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
             }
+
             var parcentage = decimal.Parse(Console.ReadLine());
             persons.ForEach(p => p.IncreaseSalary(parcentage));
             persons.ForEach(p => Console.WriteLine(p.ToString()));
-
         }
     }
 }
