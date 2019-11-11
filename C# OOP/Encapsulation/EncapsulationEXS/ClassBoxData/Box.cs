43803ea318eac3cb2c66e1245358303fc68119a9
delev <delyan1@mail.bg>
2019-11-11 15:33:04 +0200

    Add ClassBoxData solution

---
+++ C# OOP/Encapsulation/EncapsulationEXS/ClassBoxData/Box.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                if (value > 0)
+                {
+                    this.length = value; ;
+                }
+                else
+                {
+                    throw new Exception($"Length cannot be zero or negative.");
+                }
+            }
+        }
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value > 0)
+                {
+                    this.width = value; ;
+                }
+                else
+                {
+                    throw new Exception($"Width cannot be zero or negative.");
+                }
+            }
+        }
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value > 0)
+                {
+                    this.height = value; ;
+                }
+                else
+                {
+                    throw new Exception($"Height cannot be zero or negative.");
+                }
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * (this.Height * this.Width) + 2 * (this.Height * this.Length) + 2 * (this.Width * this.Length);
+        }
+
+        public double GetVolume()
+        {
+            return this.Height * this.Width * this.Length;
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            return 2 * this.Height * (this.Length + this.Width);
+        }
+
+        public override string ToString()
+        {
+            var surface = GetSurfaceArea();
+            var lateralSurface = GetLateralSurfaceArea();
+            var volume = GetVolume();
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Surface Area - {surface:f2}");
+            sb.AppendLine($"Lateral Surface Area - {lateralSurface:f2}");
+            sb.AppendLine($"Volume - {volume:f2}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/EncapsulationEXS/ClassBoxData/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var boxLenght = double.Parse(Console.ReadLine());
+            var boxWidth = double.Parse(Console.ReadLine());
+            var boxHeight = double.Parse(Console.ReadLine());
+
+            try
+            {
+                var box = new Box(boxLenght, boxWidth, boxHeight);
+                Console.WriteLine(box);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);            
+            } 
+        }
+    }
+}
