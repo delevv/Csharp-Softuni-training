bffe8c14e01a79da61622d654c2b1e114103618f
delev <delyan1@mail.bg>
2019-11-14 11:24:39 +0200

    Add Cars solution

---
+++ C# OOP/InterfacesAndAbstraction/InterfacesAndAbstractionLAB/Cars/ICar.cs
@@ -0,0 +1,10 @@
+﻿namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        string Start();
+        string Stop();
+    }
+}
---
+++ C# OOP/InterfacesAndAbstraction/InterfacesAndAbstractionLAB/Cars/Tesla.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+
+            return sb.ToString().Trim();
+        }
+    }
+}
