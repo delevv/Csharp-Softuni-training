9f6de09be195092b7eb251c6455b591f825359a0
delev <delyan1@mail.bg>
2019-11-15 14:54:06 +0200

    Add Problem 2.Multiple Implementation solution

--- C# OOP/InterfacesAndAbstraction/InterfacesAndAbstractionEXS/PersonInfo/Citizen.cs
+++ C# OOP/InterfacesAndAbstraction/InterfacesAndAbstractionEXS/PersonInfo/Citizen.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PersonInfo
+﻿namespace PersonInfo
 {
-    public class Citizen : IPerson
+    public class Citizen : IPerson, IBirthable, IIdentifiable
     {
-        public Citizen(string name, int age)
+        public Citizen(string name, int age, string id, string birthdate)
         {
             this.Name = name;
             this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
         }
 
         public string Name { get; }
 
         public int Age { get; }
+
+        public string Birthdate { get; }
+
+        public string Id { get; }
     }
 }
