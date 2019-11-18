7bfd1d3c2d6ec3537059c4566fdd6a3061dae8eb
delev <delyan1@mail.bg>
2019-11-18 11:12:20 +0200

    Add MathOperation solution

---
+++ C# OOP/Polymorphism/PolymorphismLAB/MathOperation/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int number, int secondNumber)
+        {
+            return number + secondNumber;
+        }
+
+        public double Add(double number, double secondNumber, double thirdNumber)
+        {
+            return number + secondNumber + thirdNumber;
+        }
+
+        public decimal Add(decimal number, decimal secondNumber, decimal thirdNumber)
+        {
+            return number + secondNumber + thirdNumber;
+        }
+    }
+}
