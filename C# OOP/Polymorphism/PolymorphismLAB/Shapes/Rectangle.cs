bdf47297905c9e162ef8f538d24274cd3f0bff1f
delev <delyan1@mail.bg>
2019-11-18 15:39:51 +0200

    Add Shapes solution

---
+++ C# OOP/Polymorphism/PolymorphismLAB/Shapes/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return this.width * this.height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * this.width + 2 * this.height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
