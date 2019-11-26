199a418c021e640da3d0fdeafd5c755ee99d6a9e
delev <delyan1@mail.bg>
2019-11-26 16:06:32 +0200

    Add Stealer solution

---
+++ C# OOP/ReflectionAndAttributes/ReflectionAndAttributesLAB/Stealer/Spy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] fieldNames)
+    {
+        var classType = Type.GetType(className);
+        var classFields = classType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+        var classInstance = Activator.CreateInstance(classType, new object[] { });
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"Class under investigation: {className}");
+
+        foreach (var field in classFields.Where(f => fieldNames.Contains(f.Name)))
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+}
