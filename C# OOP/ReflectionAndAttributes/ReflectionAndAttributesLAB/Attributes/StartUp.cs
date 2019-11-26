cb6d57e6f36bca1aa52437817feb627441759dd7
delev <delyan1@mail.bg>
2019-11-26 17:06:42 +0200

    Add Attributes solution

---
+++ C# OOP/ReflectionAndAttributes/ReflectionAndAttributesLAB/Attributes/StartUp.cs
@@ -0,0 +1,10 @@
+﻿[Author("Ventsi")]
+public class StartUp
+{
+    [Author("Gosho")]
+    public static void Main(string[] args)
+    {
+        var tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
