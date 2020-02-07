db5c9294b544e738daadc1321c665f3ec9715b5c
delev <delyan1@mail.bg>
2020-02-07 12:19:28 +0200

    Add subSum solution

---
+++ JS Advanced/UnitTestingAndModules/LAB/subSum.js
@@ -0,0 +1,18 @@
+function solve(array, startIndex, endIndex) {
+    if (!Array.isArray(array) || !array.every(num => typeof (num) === "number")) {
+        return NaN;
+    }
+    else if (startIndex < 0) {
+        startIndex = 0;
+    }
+    else if (endIndex > array.length - 1) {
+        endIndex = array.length - 1;
+    }
+
+    let sum = 0;
+    for (let i = startIndex; i <= endIndex; i++) {
+        sum += array[i];
+    }
+
+    return sum;
+}
