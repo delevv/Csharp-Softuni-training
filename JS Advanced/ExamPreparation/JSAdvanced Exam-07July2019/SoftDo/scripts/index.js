d2291e1c2f2a19bd13a1ba7518316847469bde07
delev <delyan1@mail.bg>
2020-02-15 16:58:58 +0200

    Add SoftDo 88/100 solution (part 1)

---
+++ JS Advanced/ExamPreparation/JSAdvanced Exam-07July2019/SoftDo/scripts/index.js
@@ -0,0 +1,82 @@
+function mySolution() {
+    let inputSection = document.getElementById("inputSection");
+
+    let senderName = inputSection.getElementsByTagName('input')[0];
+    let question = inputSection.getElementsByTagName('textarea')[0];
+    let sendButton = inputSection.getElementsByTagName('button')[0];
+
+    let pendingQuestions = document.querySelector('div[id="pendingQuestions"]');
+    let openQuestions = document.querySelector('div[id="openQuestions"]');
+
+    sendButton.addEventListener('click', (e) => {
+        if (question.value !== '') {
+
+            let pendingQuestionDiv = document.createElement('div');
+            pendingQuestionDiv.classList.add("pendingQuestion");
+            pendingQuestions.appendChild(pendingQuestionDiv);
+
+            pendingQuestionDiv.innerHTML = '<img src="./images/user.png" width="32" height="32">' +
+                `<span>${senderName.value === '' ? 'Anonymous' : senderName.value}</span>` +
+                `<p>${question.value}</p>` +
+                '<div class="actions">' +
+                '<button class="archive">Archive</button>' +
+                '<button class="open">Open</button>' +
+                '</div>';
+
+            pendingQuestionDiv.children[3].children[0].addEventListener('click', (e) => {
+                e.target.parentElement.parentElement.remove();
+            });
+
+            pendingQuestionDiv.children[3].children[1].addEventListener('click', (e) => {
+                let currDiv = e.target.parentElement.parentElement;
+                let user = currDiv.children[1].textContent;
+                let text = currDiv.children[2].textContent;
+                currDiv.remove();
+
+                let openQuestionDiv = document.createElement('div');
+                openQuestionDiv.classList.add("openQuestion");
+                openQuestionDiv.innerHTML = '<img src="./images/user.png" width="32" height="32">' +
+                    `<span>${user}</span>` +
+                    `<p>${text}</p>` +
+                    '<div class="actions">' +
+                    '<button class="reply">Reply</button>' +
+                    '</div>' +
+                    '<div class="replySection" style="display: none;">' +
+                    '<input class="replyInput" type="text" placeholder="Reply to this question here...">' +
+                    '<button class="replyButton">Send</button>' +
+                    '<ol class="reply" type="1"></ol>' +
+                    '</div>';
+
+                let replySection = openQuestionDiv.children[4];
+                let replybutton = openQuestionDiv.children[3].children[0];
+
+                replybutton.addEventListener('click', (e) => {
+
+                    if (replySection.style.display !== 'none') {
+                        replySection.style.display = 'none';
+                        replybutton.textContent = 'Reply';
+                        return;
+                    }
+                    replySection.style.display = 'block';
+                    replybutton.textContent = 'Back';
+
+                    let answerButton = replySection.children[1];
+                    answerButton.addEventListener('click', (e) => {
+                        let input = e.target.previousElementSibling;
+                        let list = e.target.nextElementSibling;
+
+                        if (input.value != '') {
+                            let li = document.createElement('li');
+                            li.textContent = input.value;
+                            list.appendChild(li);
+                            input.value = '';
+                        }
+                    });
+                })
+                openQuestions.appendChild(openQuestionDiv);
+            });
+        }
+        senderName.value = '';
+        question.value = '';
+    })
+}
