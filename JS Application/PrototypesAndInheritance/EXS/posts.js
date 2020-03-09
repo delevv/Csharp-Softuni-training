319a2b15d5aa7425180526f88e232a19195b932e
delev <delyan1@mail.bg>
2020-03-09 14:44:08 +0200

    Add posts solution

---
+++ JS Application/PrototypesAndInheritance/EXS/posts.js
@@ -0,0 +1,60 @@
+function solve() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        toString() {
+            return `Post: ${this.title}\n` +
+                `Content: ${this.content}\n`;
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            let result = super.toString();
+            result += `Rating: ${this.likes - this.dislikes}\n`;
+
+            if (this.comments.length > 0) {
+                result += 'Comments:\n';
+                result += this.comments.map(c => ` * ${c}`).join('\n');
+            }
+
+            return result.trim();
+        }
+    }
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = views;
+        }
+
+        view() {
+            this.views++;
+            return this;
+        }
+
+        toString() {
+            return super.toString() + `Views: ${this.views}`;
+        }
+    }
+
+    return {
+        Post,
+        SocialMediaPost,
+        BlogPost
+    }
+}
