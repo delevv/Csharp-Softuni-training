d65b2cdfea4d35f58c6f490f308b9795c56d87fb
delev <delyan1@mail.bg>
2020-06-01 15:58:47 +0300

    Add One-To-Many Relationship solution

---
+++ C# DB/DB Basics/TableRelations/One-To-Many Relationship.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Manufacturers(
+	ManufacturerID INT PRIMARY KEY IDENTITY(100,1),
+	[Name] VARCHAR(50) NOT NULL,
+	EstablishedOn DATE NOT NULL
+)
+
+CREATE TABLE Models(
+	ModelID INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(50) NOT NULL,
+	ManufacturerID INT NOT NULL REFERENCES Manufacturers(ManufacturerID)
+)
