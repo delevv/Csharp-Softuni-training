569b9edf3f13512485d93aadb326a315eb27bb37
delev <delyan1@mail.bg>
2020-06-26 12:39:51 +0300

    Add VillainNames solution

---
+++ C# DB/EntityFrameworkCore/ADO.NET/VillainNames/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace VillainNames
+{
+    public class StartUp
+    {
+        private const string connectionString = @"Server=.;Database=MinionsDB;Integrated Security=true;";
+
+        public static void Main(string[] args)
+        {
+            using var sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            var getVillainsQuery = @"SELECT v.[Name],
+                                            COUNT(*) AS MinionsCount
+                                     FROM Villains AS v
+                                     LEFT JOIN MinionsVillains AS mv ON mv.VillainId = v.Id
+                                     GROUP BY v.[Name]
+                                     HAVING COUNT(*) >= 3
+                                     ORDER BY MinionsCount DESC";
+
+            using var sqlCommand = new SqlCommand(getVillainsQuery, sqlConnection);
+
+            using var reader = sqlCommand.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.ToString());
+                Console.WriteLine($"{reader[0]} - {reader[1]}");
+            }
+        }
+    }
+}
