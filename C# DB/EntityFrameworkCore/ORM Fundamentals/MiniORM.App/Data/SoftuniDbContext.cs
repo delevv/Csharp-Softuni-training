7a4ba094a2c9ee4e173f08f8f9a4c55873032bcc
delev <delyan1@mail.bg>
2020-07-02 10:57:22 +0300

    Add MiniORM.App solution

---
+++ C# DB/EntityFrameworkCore/ORM Fundamentals/MiniORM.App/Data/SoftuniDbContext.cs
@@ -0,0 +1,20 @@
+﻿using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App.Data
+{
+    public class SoftuniDbContext : DbContext
+    {
+        public SoftuniDbContext(string connectionSting)
+            : base(connectionSting)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; }
+
+        public DbSet<Department> Departments { get; }
+
+        public DbSet<Project> Projects { get; }
+
+        public DbSet<EmployeeProject> EmployeesProjects { get; }
+    }
+}
---
+++ C# DB/EntityFrameworkCore/ORM Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = "Server=.;Database=MiniORM;Integrated Security=True";
+
+            var context = new SoftuniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
