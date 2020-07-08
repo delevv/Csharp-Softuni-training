fb1a50346a30bd0d7fe512db346091fb46099735
delev <delyan1@mail.bg>
2020-07-08 13:24:02 +0300

    Add FootballBetting solution using only Fluent API

---
+++ C# DB/EntityFrameworkCore/Entity Relations/FootballBetting/P03_FootballBetting.Data.Models/Bet.cs
@@ -0,0 +1,22 @@
+﻿using P03_FootballBetting.Data.Models.Enums;
+using System;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Bet
+    {
+        public int BetId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public Prediction Prediction { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+    }
+}
---
+++ C# DB/EntityFrameworkCore/Entity Relations/FootballBetting/P03_FootballBetting.Data.Models/PlayerStatistic.cs
@@ -0,0 +1,17 @@
+﻿namespace P03_FootballBetting.Data.Models
+{
+    public class PlayerStatistic
+    {
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+
+        public int PlayerId { get; set; }
+        public virtual Player Player { get; set; }
+
+        public byte ScoredGoals { get; set; }
+
+        public byte Assists { get; set; }
+
+        public byte MinutesPlayed { get; set; }
+    }
+}
