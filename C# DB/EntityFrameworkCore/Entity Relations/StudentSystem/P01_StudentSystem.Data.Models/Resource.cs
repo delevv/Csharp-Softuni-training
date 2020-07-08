04435f44f4e72f02aaf576b50d9316041e237732
delev <delyan1@mail.bg>
2020-07-08 14:41:04 +0300

    Add StudentSystem solution with Attrubutes

---
+++ C# DB/EntityFrameworkCore/Entity Relations/StudentSystem/P01_StudentSystem.Data.Models/Resource.cs
@@ -0,0 +1,25 @@
+﻿using P01_StudentSystem.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar(max)")]
+        public string Url { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
---
+++ C# DB/EntityFrameworkCore/Entity Relations/StudentSystem/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        public virtual DbSet<Course> Courses { get; set; }
+
+        public virtual DbSet<Resource> Resources { get; set; }
+
+        public virtual DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public virtual DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.;Database=StudentSystem;Integrated Security=True;");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(entity =>
+            {
+                entity.HasKey(sc => new { sc.CourseId, sc.StudentId });
+            });
+        }
+    }
+}
