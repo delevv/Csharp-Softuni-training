4105785c95ba397fe3862b445b65f62c962dc051
delev <delyan1@mail.bg>
2020-07-10 13:42:19 +0300

    Add Sales Database solution

---
+++ C# DB/EntityFrameworkCore/Code-First/Sales Database/P03_SalesDatabase/Data/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            this.Sales = new HashSet<Sale>();
+        }
+
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Quantity { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [MaxLength(250)]
+        [DefaultValue("No description")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
---
+++ C# DB/EntityFrameworkCore/Code-First/Sales Database/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Models;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+        }
+
+        public SalesContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.;Database=SalesDb;Integrated Security=true;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {    
+        }
+    }
+}
