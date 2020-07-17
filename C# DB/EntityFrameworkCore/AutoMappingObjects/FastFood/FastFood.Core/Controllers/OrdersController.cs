3393b7daf4522eafa93050f4511de149d1b25a9c
delev <delyan1@mail.bg>
2020-07-17 16:58:17 +0300

    Add FastFood project solution

---
+++ C# DB/EntityFrameworkCore/AutoMappingObjects/FastFood/FastFood.Core/Controllers/OrdersController.cs
@@ -0,0 +1,68 @@
+﻿namespace FastFood.Core.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using FastFood.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Orders;
+
+    public class OrdersController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public OrdersController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            var viewOrder = new CreateOrderViewModel
+            {
+                Items = this.context.Items
+                    .ProjectTo<CreateOrderItemViewModel>(this.mapper.ConfigurationProvider)
+                    .ToList(),
+
+                Employees = this.context.Employees
+                    .ProjectTo<CreateOrderEmployeeViewModel>(this.mapper.ConfigurationProvider)
+                    .ToList()            
+            };
+
+            return this.View(viewOrder);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateOrderInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Error", "Home");
+            }
+
+            var order = this.mapper.Map<Order>(model);
+
+            var item = this.mapper.Map<OrderItem>(model);
+            item.Order = order;
+
+            this.context.Orders.Add(order);
+            this.context.OrderItems.Add(item);
+            this.context.SaveChanges();
+
+            return this.RedirectToAction("All", "Orders");
+        }
+
+        public IActionResult All()
+        {
+            var orders = this.context.Orders
+                .ProjectTo<OrderAllViewModel>(this.mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(orders);
+        }
+    }
+}
---
+++ C# DB/EntityFrameworkCore/AutoMappingObjects/FastFood/FastFood.Core/ViewModels/Orders/CreateOrderInputModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FastFood.Core.ViewModels.Orders
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateOrderInputModel
+    {
+        [Required]
+        public string Customer { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [Required, Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
